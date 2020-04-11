e22e49aea94531ccb5de08bc35825980fef942cb
Yannick <ydjoumbi@gmail.com>
2020-04-11 03:13:57 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -11,23 +11,14 @@ end
 toadstool
 
 def link 
-  puts sentence = 
+  sentence = "puts out 'It's Dangerous To Go Alone! Take This.'"
+  puts sentence
+end
+
 
 def all_phrases
-  mario()
-  toadstool()
+  mario
+  toadstool
+  link
 end
 
-describe "link" do
-  it "puts out 'It's Dangerous To Go Alone! Take This.'" do
-    expect{link}.to output("It's Dangerous To Go Alone! Take This.\n").to_stdout
-  end
-end
-
-describe "all_phrases" do
-  it "takes in an argument and puts out the catch phrase" do
-    expect{all_phrases}.to output(/It's-a me, Mario!\n/).to_stdout
-    expect{all_phrases}.to output(/Thank You Mario! But Our Princess Is In Another Castle!\n/).to_stdout
-    expect{all_phrases}.to output(/It's Dangerous To Go Alone! Take This.\n/).to_stdout
-  end
-end
